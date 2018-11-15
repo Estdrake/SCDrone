5ab5b205de1e11265766da0c8b98cd8e511856b1
esteban chouinard <est9854@hotmail.com>
2018-11-15 17:54:21 -0500

    Navdata

--- libardrone/src/navdata_client.cpp
+++ libardrone/src/navdata_client.cpp
@@ -2,16 +2,25 @@
 #include "navdata_client.h"
 #include "config.h"
 #include "navdata_common.h"
+#include <thread>
 
-
-NavDataClient::NavDataClient()  {
+NavDataClient::NavDataClient(ConcurrentQueue< _navdata_option_t>* queue)  {
 }
 
 NavDataClient::~NavDataClient() {
 
 }
 
+std::thread NavDataClient::start()
+{
+	socket->connectToHost(WIFI_ARDRONE_IP, NAVDATA_PORT);
+	return std::thread([this] {this->run_service(); });
+}
+
 
 void NavDataClient::run_service() {
 	
+
+
+
 }
